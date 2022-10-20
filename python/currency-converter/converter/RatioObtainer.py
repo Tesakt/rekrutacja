4bb36d755aa941e271c3e6b733d77e377f9322cf
Jakub Dadzibóg <jakubdadz@wp.pl>
2022-10-20 20:28:51 +0200

    Finished currency converter

--- python/currency-converter/converter/RatioObtainer.py
+++ python/currency-converter/converter/RatioObtainer.py
@@ -14,23 +14,57 @@ class RatioObtainer:
         # This function checks if given ratio was saved today and if the file with ratios is created at all
         # should return false when file doesn't exist or if there's no today's exchange rate for given values at all
         # should return true otherwise
-        pass
+        try:
+            with open("ratios.json", "r") as f:
+                data = json.load(f)
+                for exchange in data:
+                    if exchange['base_currency'] == self.base and exchange['target_currency'] == self.target and exchange['date_fetched'] == str(datetime.date.today()):
+                        return True
+                else:
+                    return False
+        except FileNotFoundError:
+            return False
 
     def fetch_ratio(self):
         # TODO
         # This function calls API for today's exchange ratio
         # Should ask API for today's exchange ratio with given base and target currency
         # and call save_ratio method to save it
-        pass
+        url = f'https://api.exchangerate.host/convert?from={self.base}&to={self.target}'
+        response = urllib.request.urlopen(url)
+        data = json.loads(response.read())
+        self.save_ratio(data['result'])
+        return data['result']
 
     def save_ratio(self, ratio):
         # TODO
         # Should save or update exchange rate for given pair in json file
         # takes ratio as argument
         # example file structure is shipped in project's directory, yours can differ (as long as it works)
-        pass
+        try:
+            with open("ratios.json", "r") as f:
+                data = json.load(f)
+                for exchange in data:
+                    if exchange['base_currency'] == self.base and exchange['target_currency'] == self.target:
+                        exchange['ratio'] = ratio
+                        exchange['date_fetched'] = str(datetime.date.today())
+        except FileNotFoundError:
+            data = []
+        record = {
+            "base_currency": self.base, 
+            "target_currency": self.target, 
+            "date_fetched": str(datetime.date.today()),
+            "ratio": ratio
+        }
+        data.append(record)
+        with open("ratios.json", "w") as f:
+            json.dump(data, f, indent=4)
 
     def get_matched_ratio_value(self):
         # TODO
         # Should read file and receive exchange rate for given base and target currency from that file
-        pass
+        with open("ratios.json", "r") as f:
+            data = json.load(f)
+            for exchange in data:
+                if exchange['base_currency'] == self.base and exchange['target_currency'] == self.target:
+                    return exchange['ratio']
