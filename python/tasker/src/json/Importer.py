af30f246f65ac3a6f5b5cb0807d1593af04cc61c
Jakub Dadzibóg <jakubdadz@wp.pl>
2022-10-20 23:28:00 +0200

    Finished tasker.

--- python/tasker/src/json/Importer.py
+++ python/tasker/src/json/Importer.py
@@ -4,12 +4,13 @@ import json
 class Importer:
 
     def __init__(self):
-        pass
+        self.database = []
 
     def read_tasks(self):
         # TODO odczytaj plik i zdekoduj treść tutaj
-        pass
+        with open('taski.json', 'r', encoding='utf-8') as file:
+            self.database = json.load(file)
 
     def get_tasks(self):
         # TODO zwróć zdekodowane taski tutaj
-        pass
+        return self.database
